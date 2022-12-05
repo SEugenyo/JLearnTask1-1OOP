a3a0d0dccf1d28228ecc12bf83d778db0ee254c6
Eugeny Sergeev <sergeeve@inlinegroup-c.ru>
2022-12-05 13:40:25 +0300

    Rename main class

--- src/main/java/org/example/TaskTriangleHeight.java
+++ src/main/java/org/example/TaskTriangleHeight.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class TriangleHeight {
+public class TaskTriangleHeight {
     public static void main(String[] args) {
         try {
             var triangle = new TriangleInputScannerReader(new Scanner(System.in)).readTriangle();
